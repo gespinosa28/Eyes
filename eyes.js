3f042095015d2350d22d35b675780e559a7d0463
Gina Espinosa <ginyes28@gmail.com>
2023-01-05 18:45:08 -0600

    Add files via upload

---
+++ eyes.js
@@ -0,0 +1,13 @@
+const amountBalls = 2;
+const balls = document.getElementsByClassName('ball');
+
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+    
+  for (let i = 0; i <= amountBalls; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
+};
